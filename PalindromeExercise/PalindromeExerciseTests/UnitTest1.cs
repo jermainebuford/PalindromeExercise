8654a762ea5a19b643102a67c7b2adc87ac76985
jermaine buford <bufordjermaine@outlook.com>
2022-06-24 11:58:28 -0400

    finshed work

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,15 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("bob",true)]
+        [InlineData("hello",false)]
+        [InlineData("racecar",true)]
+        public void Test1(string word, bool expected)
         {
-
+            var test = new WordSmith();
+            var actual = test.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
